8a80a65817b135ee4127f407a4309817fd5bf4ab
gjasay <gabeasay@gmail.com>
2024-04-13 09:38:39 -0600

    pointer follow

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,16 +1,21 @@
 import { Scene } from "phaser";
+import { Paper_Object } from "../objects/Paper_Object";
 
 export class Game extends Scene {
   camera: Phaser.Cameras.Scene2D.Camera;
   msg_text: Phaser.GameObjects.Text;
+  paperObj: Paper_Object;
 
   constructor() {
-    super("Game");
+    super({ key: "Game", physics: { arcade: { gravity: { x: 0, y: 0 } } }});
   }
 
   create() {
+    this.paperObj = new Paper_Object(this, 100, 100);
+    this.physics.add.existing(this.paperObj);
     this.camera = this.cameras.main;
     this.scene.launch("Paper"); // Launch the Paper scene overlay
+<<<<<<< Updated upstream
     this.sound.play("delicate", { loop: true, volume: 0.2 });
 
     this.input.once("pointerdown", () => {
@@ -23,5 +28,11 @@ export class Game extends Scene {
 
   shutdown() {
     this.sound.stopAll();
+=======
+  }
+
+  update() {
+    this.paperObj.update();
+>>>>>>> Stashed changes
   }
 }
