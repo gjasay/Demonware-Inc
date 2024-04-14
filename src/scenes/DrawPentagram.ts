f049734bede4bbed441ebf95bba6c3ce7ae52c19
gjasay <gabeasay@gmail.com>
2024-04-13 22:58:19 -0600

    Started Pentagram Game

--- src/scenes/DrawPentagram.ts
+++ src/scenes/DrawPentagram.ts
@@ -21,12 +21,11 @@ export default class DrawPentagram extends PaperBaseScene {
     graphics.closePath();
     graphics.strokePath();
 
-    this.add.circle(260, 145, 5, 0xff0000);
-    this.add.circle(146, 480, 5, 0xff0000);
-    this.add.circle(430, 280, 5, 0xff0000);
-    this.add.circle(86, 280, 5, 0xff0000);
-    this.add.circle(374, 480, 5, 0xff0000);
-    
+    const point1 = this.add.circle(260, 145, 5, 0xff0000);
+    const point2 = this.add.circle(146, 480, 5, 0xff0000);
+    const point3 = this.add.circle(430, 280, 5, 0xff0000);
+    const point4 = this.add.circle(86, 280, 5, 0xff0000);
+    const point5 = this.add.circle(374, 480, 5, 0xff0000);
   }
 
   update() {}
